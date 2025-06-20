9e9a12c5f4909925d715a11c44975f2b30970de7
安音Anin <anin-interest@163.com>
2025-06-20 22:50:19 +0800

    feat: 初始化项目

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from pathlib import Path
+
+from astrbot import logger
+from astrbot.api.event import filter
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.core import AstrBotConfig
+from astrbot.core.platform import AstrMessageEvent
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+import astrbot.core.message.components as Comp
+from astrbot.core.star.filter.event_message_type import EventMessageType
+
+import yaml
+import random
+
+
+@register(
+    "astrbot_plugin_reply",
+    "安音Anin",
+    "根据特定内容回复",
+    "1.0.0",
+    "",
+)
+class ReplyPlugin(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.replys: dict = {}
+        self.reply_keys: list = []
+        with open(Path(__file__).parent / "resources/reply.yaml", "r", encoding="utf-8") as f:
+            self.replys = yaml.safe_load(f)
+            self.reply_keys = self.replys.keys()
+            f.close()
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.event_message_type(EventMessageType.ALL)
+    async def reply_handle(self, event: AstrMessageEvent):
+        """
+        根据特定内容回复
+        """
+        messages = event.get_messages()
+        reply_msgs : list = []
+        for seg in messages:
+            if isinstance(seg, Comp.Plain):
+                text = seg.text.strip()
+                reply_msgs.extend(self.search(text))
+        if reply_msgs:
+            # 回复消息
+            yield event.plain_result(reply_msgs[random.randint(0, len(reply_msgs) - 1)])
 
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+    async def search(self, text: str) -> list:
+        """
+        搜索文本是否在keys中，并返回对应的回复消息
+        """
+        result = []
+        for key in self.reply_keys:
+            if key in text:
+                result.extend(self.replys[key])
+        return result
+
